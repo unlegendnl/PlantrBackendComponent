593349840ce31f6ca2e2c0a623eac7637641ce57
Soto Conuecar,Miguel M <m.sotoconuecar@student.fontys.nl>
2021-10-02 15:06:25 +0200

    initial push, added model for the user, the dbcontext and a database connection string. the project is based on a .net core 5 web api (weatherforecast modified) used recoure: https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-5.0

---
+++ userMicroservice/userMicroservice/userMicroservice/Data/userContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using userMicroservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace userMicroservice.Data
+{
+    public class userContext : DbContext
+    {
+        public userContext(DbContextOptions<userContext> options) : base(options)
+        {
+        }
+
+        public DbSet<userModel> User { get; set; }
+    }
+}
---
+++ userMicroservice/userMicroservice/userMicroservice/Models/userModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace userMicroservice.Models
+{
+    public class userModel
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
